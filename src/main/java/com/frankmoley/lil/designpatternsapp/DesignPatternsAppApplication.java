c37e6835b40f8035b6d356e6911aa5a18d16c589
seckinkarabel <seckinkarabel@gmail.com>
2022-10-02 14:47:19 +0200

    First Commit

---
+++ src/main/java/com/frankmoley/lil/designpatternsapp/DesignPatternsAppApplication.java
@@ -0,0 +1,15 @@
+package com.frankmoley.lil.designpatternsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsAppApplication.class, args);
+    }
+
+
+}
+
